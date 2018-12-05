3687d12a57841bd399dd75bc892107496440f586
FallenAngel <the.fallen.angel.9x@gmail.com>
2018-12-05 10:33:11 +0700

    mattermost redux

--- project/src/screen/Messenger.js
+++ project/src/screen/Messenger.js
@@ -12,6 +12,7 @@ import {
     TouchableOpacity,
 } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Client4 } from "mattermost-redux/client";
 import ExToolbar from "../layout/Toolbar";
 import { AppStyle, AppColor } from "../data/Style";
 import CONSTANT from "../data/Constant";
@@ -24,6 +25,57 @@ export default class MessengerView extends Component {
         super(props);
         this.state = {
         }
+    }
+
+    componentDidMount() {
+        this.initMessengerFeature();
+    }
+
+    //Init mattermost redux
+    initMessengerFeature() {
+        Client4.setUrl("https://chat.247post.vn");
+        Client4.login("the.fallen.angel.9x@gmail.com", "Huynh01696762809")
+            .then(() => {
+                Client4.getMe()
+                    .then((data) => {
+                        console.log("Me: ", data);
+                        this.getEverything();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    //Get everything
+    getEverything() {
+        //Get user by email
+        Client4.getUserByEmail("the.fallen.angel.9x@gmail.com").then((data) => {
+            console.log("User by email:", data);
+        })
+        //Get list team
+        Client4.getTeams().then((data) => {
+            console.log("Teams list:", data);
+        })
+        //Get team by name
+        Client4.getTeamByName("cnn").then((data) => {
+            console.log("Team by name:", data);
+        })
+        //Get channel by team id
+        Client4.getChannels("on7rec1a77ncxm4o5xpohmhr4h").then((data) => {
+            console.log("Channels by team: ", data);
+        })
+        //Post a message
+        Client4.createPost({
+            channel_id: "pyxp6ryhejyabdtaag5scp4wbr",
+            message: "Angel say hi"
+        }).then((data) => {
+            console.log("Send post: ", data);
+        })
+
     }
 
     render() {
